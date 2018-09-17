6c4fffa2d869b52309b3037888266dca45c75368
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-17 13:24:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,23 @@ class Student
     self.all.find {|student| student.name == name}
   end
 
+  def self.all_students_in_grade_9
+    self.all.select {|student| student.grade.to_i==9}
+  end
+
+  def self.students_below_12th_grade
+    self.all.select {|student| student.grade.to_i<12}
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    count=0
+    self.all.select do |student| 
+      while count <= x
+      student.grade.to_i==10
+      count=+1
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
