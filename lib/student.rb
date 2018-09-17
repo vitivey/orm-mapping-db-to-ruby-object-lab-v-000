5a773fa19dd439d57de786489606b5ccbb0c3f56
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-17 13:16:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-    self.all.select do |student| 
+    self.all.select do |student|
       student.name == name
       binding.pry
     end
